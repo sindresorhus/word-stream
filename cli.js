d0cd2e90422beb1395797d491f2f03092193ddbd
Sindre Sorhus <sindresorhus@gmail.com>
2015-11-14 18:34:12 +0700

    bump deps

--- cli.js
+++ cli.js
@@ -4,12 +4,10 @@ var fs = require('fs');
 var meow = require('meow');
 var wordListPath = require('word-list');
 
-meow({
-	help: [
-		'Usage',
-		'  $ word-stream'
-	]
-});
+meow([
+	'Usage',
+	'  $ word-stream'
+]);
 
 fs.createReadStream(wordListPath).pipe(process.stdout)
 	.on('error', process.stderr.write.bind(process.stderr));
